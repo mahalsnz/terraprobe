3f19b9ca59a1e8212b4fd11bd2dcd00152538bf2
mabbettbyron <mabznz@gmail.com>
2019-06-11 12:27:44 +1200

    #9 - Provinsiong now installs DB software and create soil database with soiladmin as the user. Django connected to DB.

--- soil/soil/settings.py
+++ soil/soil/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'soil.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'soil',
+        'USER': 'soiladmin',
+        'PASSWORD': 'soiladmin',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
