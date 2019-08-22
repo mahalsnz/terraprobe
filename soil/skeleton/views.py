cb00e7a303f3e3d85d65b9d97d1ae026f7a166b5
mabbettbyron <mabbettbyron@gmail.com>
2019-08-22 16:11:12 +1200

    #47 - File uploads working and done a lot of processing and that needs to be in seperate tickets.

--- soil/skeleton/views.py
+++ soil/skeleton/views.py
@@ -6,11 +6,15 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+
+import re
+import requests
+
 # Get an instance of a logger
 import logging
 logger = logging.getLogger(__name__)
 
-from .forms import DocumentForm
+from .forms import DocumentForm#
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -24,7 +28,7 @@ def simple_upload(request):
         uploaded_file_url = fs.url(filename)
         return render(request, 'simple_upload.html', {
             'uploaded_file_url' : uploaded_file_url
-        })
+        })#
     return render(request, 'simple_upload.html')
 
 # Upload and process PSD files
@@ -40,7 +44,7 @@ def model_form_upload(request):
     else:
         form = DocumentForm()
     return render(request, 'model_form_upload.html', {
-        'form': form
+        'form': form#
     })
 
 def handle_files(f):
@@ -48,9 +52,7 @@ def handle_files(f):
     file_data = ""
     try:
         logger.error("*******processing file*****")
-        logger.error(f)
 
-        file_data
         for chunk in f.chunks():
             #logger.error(chunk.decode("utf-8"))
             file_data = file_data + chunk.decode("utf-8")
@@ -61,8 +63,58 @@ def handle_files(f):
         #    logger.error(line)
     except Exception as e:
         logger.error(e)
-    finally:
-        logger.error(file_data)
+    #finally:
+        #logger.error(file_data)
+
+    # process
+    lines = file_data.split("\n")
+    logger.error("Serial Line:" + lines[1])
+    serialfields = lines[1].split(",")
+    serialnumber = serialfields[1]
+    logger.error("Serial Number:" + serialnumber)
+
+    # TODO: Serial Number lookup for site id
+
+
+    data = {}
+    for line in lines:
+        digit = re.search("^\d", line)
+
+        if digit:
+            # If one is first element we have a new reading record
+            readingfields = line.split(",")
+
+            #logger.error("Depth:" + str(readingfields[0]))
+            if int(readingfields[0]) == 1:
+                logger.error("create new record:" + str(data))
+                # Get date part from first depth is fine. Comes in as DD/MM/YY_crap get before underscore
+                date_raw = str(readingfields[10])
+                datefields = date_raw.split("_")
+                date = datefields[0]
+                logger.error("Date:" + date)
+                data['depth1'] = str(readingfields[6])
+                data['date'] = date
+                data['created_by'] = '2'
+                data['site'] = '3'
+                data['serial_number'] = serialnumber
+                data['type'] = 1
+                #data['created_date'] = "2019-08-22T14:06:51.521917+12:00"
+            else:
+                #depthkey = 'depth' + str(readingfields[0])
+                data['depth' + str(readingfields[0])] = str(readingfields[6])
+        else:
+            if data:
+                logger.error("Post data if something in data" + str(data))
+                headers = {'contentType': 'application/json'}
+                r = requests.post('http://127.0.0.1:8000/api/reading/', headers=headers, data=data)
+                logger.error('request response' + r.text)
+                data = {}
+
+
+
+
+
+    #r = requests.post('https://httpbin.org/post', data = {'key':'value'})
 
 
 
