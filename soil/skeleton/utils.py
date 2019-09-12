31e362783529c43bf661f0fef002d9e9f3dc77d5
mabbettbyron <mabbettbyron@gmail.com>
2019-09-12 15:17:23 +1200

    #44 - Start moving re-usable code into utils.py for now

---
+++ soil/skeleton/utils.py
@@ -0,0 +1,71 @@
+import requests
+
+# Get an instance of a logger
+import logging
+logger = logging.getLogger(__name__)
+
+from .models import Site
+
+'''
+    process_probe_data - Should be able to process data (readings dictionary) from both netron and diviner probes
+'''
+
+def process_probe_data(readings, serial_unique_id, request):
+
+    for key, site_info in readings.items():
+        # Firstly we total up each site-dates readings
+        totals = {}
+        split_key = key.split(",")
+
+        for depth_arr in site_info:
+            for index in range(len(depth_arr)):
+                print(depth_arr[index])
+                if index in totals:
+                    totals[index] = int(totals[index]) + int(depth_arr[index])
+                else:
+                    totals[index] = int(depth_arr[index])
+
+        # Secondly we average out each reading from the amount of readings taken
+        averaged_totals = []
+        readings_taken = len(site_info)
+        for key, value in totals.items():
+            #print("value:" + str(value) + " readings_taken:" + str(readings_taken))
+            averaged_totals.append(int(value) / int(readings_taken))
+
+        # Thirdly we reverse thate order of averaged_totals
+        averaged_totals.reverse()
+        #print(averaged_totals)
+
+        # create data object in the way we want
+        data = {}
+        data['date'] = split_key[1]
+
+        # Site is the primary key of site number so we need to look it up.
+        s = Site.objects.get(site_number=split_key[0])
+        data['site'] = s.id
+
+        # TODO: When we have authorization over site set up can have logged in user as created by
+        data['created_by'] = '2'
+
+        data['serial_number'] = serial_unique_id
+        data['type'] = '1' # always probe
+
+        for index in range(len(averaged_totals)):
+            data['depth' + str(index + 1)] = averaged_totals[index]
+
+        logger.error("Ready to insert:" + str(data))
+
+        if data:
+            # TODO: Add unique key on readings table for date, reading_type and site
+            logger.error("Post data if something in data" + str(data))
+            host = request.get_host()
+            headers = {'contentType': 'application/json'}
+            r = requests.post('http://' + host + '/api/reading/', headers=headers, data=data)
+            try:
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                logger.error('request response' + r.text)
+                raise Exception(r.text)
+            data = {}
+
+    logger.error("Outside of Process Data Loop:")
--- soil/skeleton/views.py
+++ soil/skeleton/views.py
@@ -20,6 +20,8 @@ logger = logging.getLogger(__name__)
 from .forms import DocumentForm#
 from datetime import datetime
 
+from .utils import process_probe_data
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
@@ -191,70 +193,6 @@ def handle_probe_file(file_data, request):
     data[key].append(readings) # Always insert last reading
     logger.error("Final Data:" + str(data))
     process_probe_data(data, p.id, request)
-
-'''
-    process_probe_data
-'''
-
-def process_probe_data(readings, serial_unique_id, request):
-
-    for key, site_info in readings.items():
-        # Firstly we total up each site-dates readings
-        totals = {}
-        split_key = key.split(",")
-
-        for depth_arr in site_info:
-            for index in range(len(depth_arr)):
-                print(depth_arr[index])
-                if index in totals:
-                    totals[index] = int(totals[index]) + int(depth_arr[index])
-                else:
-                    totals[index] = int(depth_arr[index])
-
-        # Secondly we average out each reading from the amount of readings taken
-        averaged_totals = []
-        readings_taken = len(site_info)
-        for key, value in totals.items():
-            #print("value:" + str(value) + " readings_taken:" + str(readings_taken))
-            averaged_totals.append(int(value) / int(readings_taken))
-
-        # Thirdly we reverse thate order of averaged_totals
-        averaged_totals.reverse()
-        #print(averaged_totals)
-
-        # create data object in the way we want
-        data = {}
-        data['date'] = split_key[1]
-
-        # Site is the primary key of site number so we need to look it up.
-        s = Site.objects.get(site_number=split_key[0])
-        data['site'] = s.id
-
-        # TODO: When we have authorization over site set up can have logged in user as created by
-        data['created_by'] = '2'
-
-        data['serial_number'] = serial_unique_id
-        data['type'] = '1' # always probe
-
-        for index in range(len(averaged_totals)):
-            data['depth' + str(index + 1)] = averaged_totals[index]
-
-        logger.error("Ready to insert:" + str(data))
-
-        if data:
-            # TODO: Add unique key on readings table for date, reading_type and site
-            logger.error("Post data if something in data" + str(data))
-            host = request.get_host()
-            headers = {'contentType': 'application/json'}
-            r = requests.post('http://' + host + '/api/reading/', headers=headers, data=data)
-            try:
-                r.raise_for_status()
-            except requests.exceptions.HTTPError as e:
-                logger.error('request response' + r.text)
-                raise Exception(r.text)
-            data = {}
-
-    logger.error("Outside of Process Data Loop:")
 
 '''
     handle_diviner_file
