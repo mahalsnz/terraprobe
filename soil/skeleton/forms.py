715bae88d349c518abec6f3ae84a17d54794a8eb
mabbettbyron <mabbettbyron@gmail.com>
2019-11-06 07:23:54 +1300

    #43 - Added tecnician to selector.

--- soil/skeleton/forms.py
+++ soil/skeleton/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Document, Site
+from django.contrib.auth.models import User
 from django.forms import ModelChoiceField
 
 class DocumentForm(forms.ModelForm):
@@ -8,9 +9,20 @@ class DocumentForm(forms.ModelForm):
         model = Document
         fields = ['description', 'document', 'created_date', 'created_by']
 
+class UserModelChoiceField(forms.ModelChoiceField):
+    """
+    Extend ModelChoiceField for users so that the choices are
+    listed as 'first_name last_name (username)' instead of just
+    'username'.
+
+    """
+    def label_from_instance(self, obj):
+        return "%s (%s)" % (obj.get_full_name(), obj.username)
+
 class SelectorForm(forms.Form):
     #site = forms.CharField(label='Site', max_length=100)to_field_name="id",
     site = forms.ModelChoiceField(queryset=Site.objects.all(), widget=forms.Select(attrs={"onChange":'submit()'}))
+    technician = forms.UserModelChoiceField(queryset=User.objects.filter(groups__name='Technician'), widget=forms.Select(attrs={"onChange":'submit()'}))
     #days = forms.ModelChoiceField(queryset=Day.objects.all(), widget=forms.Select(attrs={"onChange":'refresh()'}))
 
     class Meta:
