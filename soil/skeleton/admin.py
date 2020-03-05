7178150e53059d5fa90a11712cf86635d7fb595f
mabbettbyron <mabbettbyron@gmail.com>
2020-03-06 11:20:42 +1300

    #120 - Added the two tables and some test data.

--- soil/skeleton/admin.py
+++ soil/skeleton/admin.py
@@ -16,7 +16,7 @@ from .models import CriticalDateType
 from .models import CriticalDate, UserFullName
 from .models import SeasonStartEnd
 from .models import WeatherStation
-from .models import Variety
+from .models import Variety, Strategy, StrategyType
 
 class WeatherStationAdmin(admin.ModelAdmin):
     list_display = ('name', 'region', 'code')
@@ -61,7 +61,7 @@ class SiteAdmin(admin.ModelAdmin):
         ('Depths',  {'fields': [('depth1', 'depth_he1'),('depth2', 'depth_he2'),('depth3', 'depth_he3'),('depth4', 'depth_he4'),
                                 ('depth5', 'depth_he5'),('depth6', 'depth_he6'),('depth7', 'depth_he7'),('depth8', 'depth_he8'),
                                 ('depth9', 'depth_he9'),('depth10', 'depth_he10')],'classes': ['collapse']}),
-        ('Schedule',    {'fields': ['upper_limit', 'lower_limit', 'emitter_rate', 'row_spacing', 'emitter_spacing', 'plant_spacing', 'wetted_width', 'delivery_time', 'area'],
+        ('Schedule',    {'fields': ['upper_limit', 'upper_strategy', 'lower_limit', 'lower_strategy', 'emitter_rate', 'row_spacing', 'emitter_spacing', 'plant_spacing', 'wetted_width', 'delivery_time', 'area'],
             'classes': ['collapse']}),
     ]
     radio_fields = {'irrigation_method': admin.HORIZONTAL}
@@ -106,3 +106,5 @@ admin.site.register(CriticalDate, CriticalDateAdmin)
 admin.site.register(SeasonStartEnd, SeasonStartEndAdmin)
 admin.site.register(WeatherStation, WeatherStationAdmin)
 admin.site.register(Variety)
+admin.site.register(Strategy)
+admin.site.register(StrategyType)
--- soil/skeleton/models.py
+++ soil/skeleton/models.py
@@ -51,10 +51,29 @@ class Report(models.Model):
     def __str__(self):
         return self.name
 
+class Season(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    current_flag = models.BooleanField(default=None, null=True, help_text="Is this the current season, only one season can be the current season")
+    created_date = models.DateTimeField('date published', default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,default=User)
+
+    def __str__(self):
+        return self.name
+
 class WeatherStation(models.Model):
     region = models.ForeignKey('address.State', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True)
     code = models.CharField(max_length=4, null=True)
+
+    def __str__(self):
+        return self.name
+
+class CriticalDateType(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    season_flag = models.BooleanField()
+    comment = models.CharField(max_length=200, null=True, blank=True)
+    created_date = models.DateTimeField('date published', default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,default=User)
 
     def __str__(self):
         return self.name
@@ -107,6 +126,33 @@ class Crop(models.Model):
 
     class Meta:
         unique_together = (('name', 'variety'))
+
+class StrategyType(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    comment = models.TextField(null=True, blank=True)
+
+    created_date = models.DateTimeField('date published', default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+
+    def __str__(self):
+        return self.name
+
+class Strategy(models.Model):
+    type = models.ForeignKey(StrategyType, null=False, on_delete=models.CASCADE)
+    critical_date_type = models.ForeignKey(CriticalDateType, null=False, on_delete=models.CASCADE)
+    days = models.IntegerField(null=False, help_text="A positive or negative number indicated the amount of days away from that critical date.")
+    percentage = models.FloatField(null=False, help_text="A number beween 1 and 0 indicating the variation from the limit associated with the strategy.")
+    comment = models.TextField(null=True, blank=True)
+
+    created_date = models.DateTimeField('date published', default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+
+    def __str__(self):
+        type = str(self.type)
+        critical_date_type = str(self.critical_date_type)
+        days = str(self.days)
+        percentage = str(self.percentage)
+        return type + ":" + critical_date_type + ":" + days + ":" + percentage
 
 class Site(models.Model):
     # Main
@@ -164,7 +210,9 @@ class Site(models.Model):
 
     #Scheduling
     upper_limit = models.ForeignKey(ReadingType, related_name="upper_limit_type", null=True, blank=True, on_delete=models.CASCADE, help_text="Target Upper line for Graph")
+    upper_strategy = models.ForeignKey(StrategyType, related_name="upper_strategy_type", null=True, blank=True, on_delete=models.CASCADE, help_text="Strategy dictating deviance of Upper line for Graph")
     lower_limit = models.ForeignKey(ReadingType, related_name="lower_limit_type", null=True, blank=True, on_delete=models.CASCADE, help_text="Target Lower line for Graph")
+    lower_strategy = models.ForeignKey(StrategyType, related_name="lower_strategy_type", null=True, blank=True, on_delete=models.CASCADE, help_text="Strategy dictating deviance of Lower line for Graph")
     emitter_rate = models.FloatField(null=True, blank=True)
     row_spacing = models.IntegerField(null=True, blank=True, verbose_name="Row Spacing (cm)")
     emitter_spacing = models.IntegerField(null=True, blank=True, verbose_name="Emitter Spacing (cm)")
@@ -177,33 +225,6 @@ class Site(models.Model):
 
     created_date = models.DateTimeField('date published', default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
-
-    def __str__(self):
-        return self.name
-
-# Combines Site name and number. A lot of sites are known by number
-class SiteDescription(Site):
-    class Meta:
-        proxy = True
-
-    def __str__(self):
-        return "(" + self.site_number + ") " + self.name
-
-class Season(models.Model):
-    name = models.CharField(max_length=20, null=False)
-    current_flag = models.BooleanField(default=None, null=True, help_text="Is this the current season, only one season can be the current season")
-    created_date = models.DateTimeField('date published', default=timezone.now)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE,default=User)
-
-    def __str__(self):
-        return self.name
-
-class CriticalDateType(models.Model):
-    name = models.CharField(max_length=50, null=False)
-    season_flag = models.BooleanField()
-    comment = models.CharField(max_length=200, null=True, blank=True)
-    created_date = models.DateTimeField('date published', default=timezone.now)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE,default=User)
 
     def __str__(self):
         return self.name
@@ -224,6 +245,14 @@ class CriticalDate(models.Model):
 
     class Meta:
         unique_together = (('site', 'season', 'type'))
+
+# Combines Site name and number. A lot of sites are known by number
+class SiteDescription(Site):
+    class Meta:
+        proxy = True
+
+    def __str__(self):
+        return "(" + self.site_number + ") " + self.name
 
 class SeasonStartEnd(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
