fc568c46d7e844fddfe3176adbee7ee2d47dbe51
Mahalsnz <mahalsnz@gmail.com>
2020-09-30 14:53:33 +1300

    #161 - Nailing down summary api

--- soil/graphs/apiviews.py
+++ soil/graphs/apiviews.py
@@ -9,6 +9,7 @@ from .serializers import VSWSerializer, SiteSerializer, FarmSerializer, ReadingT
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.serializers import serialize
+from skeleton.utils import get_current_season, get_site_season_start_end
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -28,22 +29,39 @@ class FruitionSummary(APIView):
         logger.debug(str(request.GET))
         ids = request.GET.getlist('sites[]')
         logger.debug(ids)
+        season = get_current_season()
         strategy_serialized_data = []
         for site_id in ids:
             try:
                 site = Site.objects.get(pk=site_id)
-                logger.debug(str(site.name))
-                latest_reading = vsw_reading.objects.filter(site=site.id, reviewed=True).latest('date')
-                logger.debug('Latest Date:' + str(latest_reading.date) + ' RZ1:' + str(latest_reading.rz1))
+                latest_reading = vsw_reading.objects.filter(site=site.id, type='Probe', reviewed=True).latest('date')
+                logger.debug('Latest Date for site :' + str(site.name) + ' - ' + str(latest_reading.date) + ' RZ1:' + str(latest_reading.rz1))
+
+                # Get sites full point and refill readings for season
+                dates = get_site_season_start_end(site, season)
+                full = vsw_reading.objects.get(site_id=site_id, type='Full Point', date__range=(dates.period_from, dates.period_to))
+                refill = vsw_reading.objects.get(site_id=site_id, type='Refill', date__range=(dates.period_from, dates.period_to))
+                logger.debug('Full Point rz1:' + str(full.rz1) + ' Refill rz1:' + str(refill.rz1))
+
                 strategy = vsw_strategy.objects.filter(site=latest_reading.site).filter(Q(strategy_date__gte=latest_reading.date)).order_by('strategy_date')[0]
-                logger.debug('Strategy:' + str(strategy.strategy_date))
+                logger.debug('Strategy:' + str(strategy.strategy_date) + ' percentage ' + str(strategy.percentage) )
+
+                # Calculation
+                diff = full.rz1 - refill.rz1
+                upper = full.rz1 - ( diff - ( diff * strategy.percentage ))
+                lower = upper - ( diff * strategy.strategy_percentage )
+
+                logger.debug('Upper:' + str(upper) + ' Lower:' + str(lower))
+
                 strategy_serialized_data.append({
                     'site_id': site.id,
                     'latest_reading_date': latest_reading.date,
                     'latest_reading_date_rz1': latest_reading.rz1,
-                    'stategy_date': strategy.strategy_date,
-                    'strategy_rz1': strategy.rz1,
-                    'percentage': strategy.percentage
+                    'strategy_date': strategy.strategy_date,
+                    'full_point': full.rz1,
+                    'refill_point': refill.rz1,
+                    'strategy_max' : round(upper, 1),
+                    'strategy_min' : round(lower, 1)
                 })
 
             except ObjectDoesNotExist:
