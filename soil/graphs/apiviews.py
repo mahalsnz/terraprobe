aca8b33ee2c91a340012443983af47743c43479a
mabbettbyron <mabbettbyron@gmail.com>
2020-10-30 11:41:55 +1300

    #200 - Chanages to summary around calculation of stratregy point and alert levels.

--- soil/graphs/apiviews.py
+++ soil/graphs/apiviews.py
@@ -43,25 +43,95 @@ class FruitionSummary(APIView):
                 refill = vsw_reading.objects.get(site_id=site_id, type='Refill', date__range=(dates.period_from, dates.period_to))
                 logger.debug('Full Point rz1:' + str(full.rz1) + ' Refill rz1:' + str(refill.rz1))
 
-                strategy = vsw_strategy.objects.filter(site=latest_reading.site).filter(Q(strategy_date__gte=latest_reading.date)).order_by('strategy_date')[0]
-                logger.debug('Strategy:' + str(strategy.strategy_date) + ' percentage ' + str(strategy.percentage) )
+                # get the strategy on either side of the latest reading date
+                after_strategy = vsw_strategy.objects.filter(site=latest_reading.site).filter(Q(strategy_date__gte=latest_reading.date)).order_by('strategy_date')[0]
+                logger.debug('After Strategy:' + str(after_strategy.strategy_date) + ' percentage ' + str(after_strategy.percentage) )
+                before_strategy = vsw_strategy.objects.filter(site=latest_reading.site).filter(Q(strategy_date__lte=latest_reading.date)).order_by('-strategy_date')[0]
+                logger.debug('Before Strategy:' + str(before_strategy.strategy_date) + ' percentage ' + str(before_strategy.percentage) )
+
+                # Get the days between before and after
+                days_between_strategies = (after_strategy.strategy_date - before_strategy.strategy_date).days
+                logger.debug('Days between strategies:' + str(days_between_strategies))
 
                 # Calculation
                 diff = full.rz1 - refill.rz1
-                upper = full.rz1 - ( diff - ( diff * strategy.percentage ))
-                lower = upper - ( diff * strategy.strategy_percentage )
+                before_upper = full.rz1 - ( diff - ( diff * before_strategy.percentage ))
+                after_upper = full.rz1 - ( diff - ( diff * after_strategy.percentage ))
+                logger.debug('Before Upper:' + str(before_upper) + ' After Upper:' + str(after_upper) )
+                diff_upper = after_upper - before_upper
+                logger.debug('Difference between Upper Strategies:' + str(diff_upper))
+                day_change_upper = diff_upper / days_between_strategies
+                logger.debug('Day change for Upper:' + str(day_change_upper))
 
-                logger.debug('Upper:' + str(upper) + ' Lower:' + str(lower))
+                before_lower = before_upper - ( diff * before_strategy.strategy_percentage )
+                after_lower = after_upper - ( diff * before_strategy.strategy_percentage )
+                logger.debug('Before Lower:' + str(before_lower) + ' After Lower:' + str(after_lower) )
+                diff_lower = after_lower - before_lower
+                logger.debug('Difference between Lower Strategies:' + str(diff_lower))
+                day_change_lower = diff_lower / days_between_strategies
+                logger.debug('Day change for Lower:' + str(day_change_lower))
+
+                # Days between the before strategy and the reading date
+                days_between_before = (latest_reading.date - before_strategy.strategy_date).days
+                logger.debug('days_between_before:' + str(days_between_before))
+                upper_strategy_change = days_between_before * day_change_upper
+                logger.debug('Upper Stategy Change:' + str(upper_strategy_change))
+                lower_strategy_change = days_between_before * day_change_lower
+                logger.debug('Lower Stategy Change:' + str(lower_strategy_change))
+
+                # Add change to original before upper and before lower
+                strategy_max = upper_strategy_change + before_upper
+                strategy_min = lower_strategy_change + before_lower
+
+                outside_limits = False
+                outside_strategy = False
+                if latest_reading.rz1 > full.rz1 or latest_reading.rz1 < refill.rz1:
+                    outside_limits = True
+                if latest_reading.rz1 > strategy_max or latest_reading.rz1 < strategy_min:
+                    outside_strategy = True
+                logger.debug('outside_limits:' + str(outside_limits) + ' outside_strategy:' + str(outside_strategy))
+
+                alert_level = 0
+                if outside_limits and outside_strategy:
+                    alert_level = 3
+                elif outside_limits and not outside_strategy:
+                    alert_level = 2
+                elif not outside_limits and outside_strategy:
+                    alert_level = 2
+                else:
+                    alert_level = 1
+                '''
+                export const CalculateSiteSummaryLevel = (summary) => {
+  let outsiteLimits = false;
+  let outsiteStrategy = false;
+  if (
+    summary.latest_reading_date_rz1 > summary.full_point ||
+    summary.latest_reading_date_rz1 < summary.refill_point
+  )
+    outsiteLimits = true;
+  if (
+    summary.latest_reading_date_rz1 > summary.strategy_max ||
+    summary.latest_reading_date_rz1 < summary.strategy_min
+  )
+    outsiteStrategy = true;
+  return outsiteStrategy && outsiteLimits
+    ? 3
+    : !outsiteStrategy && outsiteLimits
+    ? 2
+    : outsiteStrategy && !outsiteLimits
+    ? 2
+    : 1;
+    '''
 
                 strategy_serialized_data.append({
                     'site_id': site.id,
                     'latest_reading_date': latest_reading.date,
                     'latest_reading_date_rz1': latest_reading.rz1,
-                    'strategy_date': strategy.strategy_date,
                     'full_point': full.rz1,
                     'refill_point': refill.rz1,
-                    'strategy_max' : round(upper, 1),
-                    'strategy_min' : round(lower, 1)
+                    'strategy_max' : round(strategy_max, 2),
+                    'strategy_min' : round(strategy_min, 2),
+                    'alert_level' : alert_level
                 })
 
             except ObjectDoesNotExist:
