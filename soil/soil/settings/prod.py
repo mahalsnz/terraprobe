220285da6bb82c4f8fb0c29e5f0de66afeb1a867
mabbettbyron <mabbettbyron@gmail.com>
2020-12-08 14:44:12 +1300

    #209 - Upped processes for UWSGI

--- soil/soil/settings/prod.py
+++ soil/soil/settings/prod.py
@@ -8,7 +8,7 @@ SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 CORS_ORIGIN_ALLOW_ALL = True
 
-LOGLEVEL = os.environ.get('LOGLEVEL', 'debug').upper()
+LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
 
 DATABASES = {
     'default': {
